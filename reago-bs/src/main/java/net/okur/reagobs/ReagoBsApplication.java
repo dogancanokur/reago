5d0acda9a9be940301b8a2af451e9d602c1aee10
dogancanokur <dogancanokur@gmail.com>
2023-10-16 00:50:30 +0300

    init project

---
+++ reago-bs/src/main/java/net/okur/reagobs/ReagoBsApplication.java
@@ -0,0 +1,13 @@
+package net.okur.reagobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReagoBsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ReagoBsApplication.class, args);
+  }
+
+}
---
+++ reago-bs/src/test/java/net/okur/reagobs/ReagoBsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.okur.reagobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReagoBsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
