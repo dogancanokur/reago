19b4aa30fb334e6caebbebffc13a38d4a9e1ce63
dogancanokur <dogancanokur@gmail.com>
2023-10-22 04:23:33 +0300

    java mail sender

--- reago-bs/src/main/java/net/okur/reagobs/service/impl/UserServiceImpl.java
+++ reago-bs/src/main/java/net/okur/reagobs/service/impl/UserServiceImpl.java
@@ -1,16 +1,22 @@
 package net.okur.reagobs.service.impl;
 
+import jakarta.transaction.Transactional;
 import net.okur.reagobs.dto.input.UserInput;
 import net.okur.reagobs.dto.output.UserOutput;
 import net.okur.reagobs.entity.User;
 import net.okur.reagobs.repository.UserRepository;
 import net.okur.reagobs.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Properties;
 import java.util.UUID;
 
 @Service
@@ -31,12 +37,14 @@ public class UserServiceImpl implements UserService {
   }
 
   @Override
+  @Transactional(value = Transactional.TxType.REQUIRED, rollbackOn = { MailException.class })
   public UserOutput createUser(UserInput userInput) {
     User user = userInput.toUser();
     String encodedPassword = passwordEncoder.encode(user.getPassword());
     user.setActivationToken(String.valueOf(UUID.randomUUID()));
     user.setPassword(encodedPassword);
     user = userRepository.save(user);
+    sendActivationEmail(user);
     return new UserOutput(user.getId(), userInput.username(), user.getEmail(), user.getActive());
   }
 
@@ -50,4 +58,26 @@ public class UserServiceImpl implements UserService {
     userRepository.deleteById(id);
   }
 
+  private void sendActivationEmail(User user) {
+    SimpleMailMessage mailMessage = new SimpleMailMessage();
+    mailMessage.setFrom("noreply@reago.com");
+    mailMessage.setTo(user.getEmail());
+    mailMessage.setSubject("Account Activation");
+    mailMessage.setText("http://localhost:5173/activation/" + user.getActivationToken());
+
+    getJavaMailSender().send(mailMessage);
+  }
+
+  public JavaMailSender getJavaMailSender() {
+    JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+    javaMailSender.setHost("smtp.ethereal.email");
+    javaMailSender.setPort(587);
+    javaMailSender.setUsername("verner84@ethereal.email");
+    javaMailSender.setPassword("n2qyMaCGnefr2DgyCf");
+
+    Properties properties = javaMailSender.getJavaMailProperties();
+    properties.put("mail.smtp.starttls.enable", "true");
+
+    return javaMailSender;
+  }
 }
