8d0e090b2eb12a024344d4e84aef2f2fd1ae532a
dogancanokur <dogancanokur@gmail.com>
2023-10-22 05:14:33 +0300

    application settings

--- reago-bs/src/main/java/net/okur/reagobs/mail/EmailService.java
+++ reago-bs/src/main/java/net/okur/reagobs/mail/EmailService.java
@@ -1,7 +1,9 @@
 package net.okur.reagobs.mail;
 
+import jakarta.annotation.PostConstruct;
 import net.okur.reagobs.service.TranslateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
@@ -10,16 +12,29 @@ import java.util.Properties;
 
 @Service
 public class EmailService {
+  @Value("${reago.smtp.host}")
+  String SMTP_HOST;
+
+  @Value("${reago.smtp.port}")
+  int SMTP_PORT;
+
+  @Value("${reago.smtp.username}")
+  String SMTP_USERNAME;
+
+  @Value("${reago.smtp.password}")
+  String SMTP_PASSWORD;
+
   JavaMailSenderImpl javaMailSender;
   @Autowired
   TranslateService translateService;
 
+  @PostConstruct
   private void initialize() {
     this.javaMailSender = new JavaMailSenderImpl();
-    javaMailSender.setHost("smtp.ethereal.email");
-    javaMailSender.setPort(587);
-    javaMailSender.setUsername("verner84@ethereal.email");
-    javaMailSender.setPassword("n2qyMaCGnefr2DgyCf");
+    javaMailSender.setHost(SMTP_HOST);
+    javaMailSender.setPort(SMTP_PORT);
+    javaMailSender.setUsername(SMTP_USERNAME);
+    javaMailSender.setPassword(SMTP_PASSWORD);
 
     Properties properties = javaMailSender.getJavaMailProperties();
     properties.put("mail.smtp.starttls.enable", "true");
