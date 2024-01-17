8ea2133b9df4098c55e907263a517614915e490e
dogancanokur <dogancanokur@gmail.com>
2024-01-18 01:44:14 +0300

    fe - Update configuration files and dependencies

--- fe/.eslintrc.cjs
+++ fe/.eslintrc.cjs
@@ -2,22 +2,22 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:react/jsx-runtime',
-      'plugin:react-hooks/recommended',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
   ],
-    ignorePatterns: ['dist', '.eslintrc.cjs'],
-    parserOptions: {ecmaVersion: 'latest', sourceType: 'module'},
-    settings: {react: {version: '18.2'}},
-    parser: '@typescript-eslint/parser',
-    plugins: ['react-refresh', 'react-hooks'],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'react-hooks'],
   rules: {
-      'react-refresh/only-export-components': [
-          'warn',
+    'react-refresh/only-export-components': [
+      'warn',
       { allowConstantExport: true },
     ],
-      'react-hooks/rules-of-hooks': 'error', // Hook kurallarını kontrol eder
-      'react-hooks/exhaustive-deps': 'warn', // Efekt bağımlılıklarını kontrol eder
+    'react-hooks/rules-of-hooks': 'error', // Hook kurallarını kontrol eder
+    'react-hooks/exhaustive-deps': 'warn', // Efekt bağımlılıklarını kontrol eder
   },
 };
